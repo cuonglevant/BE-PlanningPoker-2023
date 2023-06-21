2b0beb36bcd3c1209e96d8331015a615502975e2
Azanamin <minhnhat912002@gmail.com>
2023-06-21 17:48:04 +0700

    chore: update constant define files

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const CLIENT_URL = process.env.DEV_CLIENT_URL || 'http://localhost:3000';
 const SERVER_URL = process.env.DEV_SERVER_URL || 'http://localhost:8080';
 const DB_CONNECTION_STRING =
@@ -5,6 +8,7 @@ const DB_CONNECTION_STRING =
 const PORT = process.env.PORT || 8080;
 const CLIENT_ID = process.env.CLIENT_ID || '';
 const CLIENT_SECRET = process.env.CLIENT_SECRET || '';
+const SESSION_SECRET = 'poker-secret';
 
 export {
   CLIENT_ID,
@@ -13,4 +17,5 @@ export {
   DB_CONNECTION_STRING,
   PORT,
   SERVER_URL,
+  SESSION_SECRET,
 };
