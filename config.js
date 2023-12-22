e8cdf5d93d74530195f5a65680bc8f0de0ffe86d
CuongLevant <cuonglnv.21it@vku.udn.vn>
2023-12-22 12:59:41 +0700

    fix url

--- config.js
+++ config.js
@@ -5,9 +5,10 @@ dotenv.config();
 export const CLIENT_URL = process.env.DEV_CLIENT_URL || 'http://localhost:3000';
 
 export const HOSTED_CLIENT_URL =
-  process.env.HOSTED_CLIENT_URL || 'https://soft-marzipan-5330c9.netlify.app';
+  process.env.HOSTED_CLIENT_URL || 'https://cesplanningpoker.netlify.app';
 
-export const SERVER_URL = process.env.DEV_SERVER_URL || 'http://localhost:8080';
+export const SERVER_URL =
+  process.env.DEV_SERVER_URL || 'be-ces-intern-2023.onrender.com';
 
 export const DB_CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017';
